b2e96d454ff6a4a4ab89b87fde87818a6afffb18
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-18 17:37:07 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-state-backend-vamsee"
     key          = "terraform/image-resize/statefile"
-    region       = "us-east-1"
+    region       = "us-west-2"
     encrypt      = true
     use_lockfile = true
   }
